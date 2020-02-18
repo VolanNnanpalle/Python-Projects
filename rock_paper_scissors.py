5e6086d4a8f3bb7a9bb0aba657c4e804a4ac7311
Volan Nnanpalle <Volan@Volans-MBP.home>
2020-02-18 00:02:58 -0500

    starting rock, paper scissors game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,6 @@
+"""
+
+Rock, Paper, Scissors Game
+Make a rock-paper-scissors game where it is the player vs the computer. The computer’s answer will be randomly generated, while the program will ask the user for their input.
+
+""
